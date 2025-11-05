0e24db2a4b49df10a891e4ff671dd783103bb252
ming2 <ming2@Mingders-Mac-mini.local>
2025-11-06 00:03:55 +0800

    init

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Button } from "@yourorg/my-react-lib"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Button />
+  </React.StrictMode>
+)
+
